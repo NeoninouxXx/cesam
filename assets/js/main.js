9c023732e65fe8b20b91b9524df2b49626633806
xavier turpin <turpin182@gmail.com>
2023-01-28 11:25:44 +0400

    update du site complet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
-const closeIcone = document.querySelector(".bi-x-lg");
-const openIcone = document.querySelector(".navbar-toggler-icon");
-closeIcone.style.display = "none";
+const navbarToggleButton = document.querySelector(".navbar-toggler");
+const navbarToggleIcon = document.querySelector(".navbar-toggler-icon");
+const iconClose = document.querySelector(".close");
+let toggle = false;
+const navLink = document.querySelector(".nav-link");
 
-closeIcone.addEventListener("click", () => {
-  closeIcone.style.display = "none";
-  openIcone.style.display = "block";
-});
+iconClose.style.display = "none";
 
-openIcone.addEventListener("click", () => {
-  closeIcone.style.display = "block";
-  openIcone.style.display = "none";
-});
+navbarToggleButton.addEventListener("click", swipeIcone);
+
+function swipeIcone() {
+  toggle = !toggle;
+  if (toggle) {
+    iconClose.style.display = "block";
+    navbarToggleIcon.style.display = "none";
+  } else {
+    iconClose.style.display = "none";
+    navbarToggleIcon.style.display = "block";
+  }
+}
