334db4be65ad6dc17f2052e614c3e9dae1ffb9f8
xavier turpin <turpin182@gmail.com>
2023-02-21 18:53:16 +0400

    gestion navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,8 @@ const navbarToggleIcon = document.querySelector(".navbar-toggler-icon");
 const iconClose = document.querySelector(".close");
 let toggle = false;
 
-
+// gestion du boutton de la navbar
 iconClose.style.display = "none";
-
-
-
 
 navbarToggleButton.addEventListener("click", swipeIcone);
 
@@ -21,3 +18,25 @@ function swipeIcone() {
     navbarToggleIcon.style.display = "block";
   }
 }
+
+// gestion de l'affichage de la navbar
+
+let prevScrollPos = window.pageYOffset;
+
+function fixedNav() {
+  const currentScrollPos = window.pageYOffset;
+  const navbar = document.querySelector(".navbar");
+
+
+  if (currentScrollPos < prevScrollPos) {
+    navbar.classList.add("fixed-top");
+    navbar.classList.add("navbar-spawn");
+  } else {
+    navbar.classList.remove("fixed-top");
+    navbar.classList.remove("navbar-spawn");
+  }
+
+  prevScrollPos = currentScrollPos;
+}
+
+window.addEventListener("scroll", fixedNav);
