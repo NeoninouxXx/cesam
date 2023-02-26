86a9e44cfea2b367835a2be9e03f4205405632bc
xavier turpin <turpin182@gmail.com>
2023-02-26 19:59:34 +0400

    last update

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,14 @@ iconClose.style.display = "none";
 navbarToggleButton.addEventListener("click", swipeIcone);
 
 function swipeIcone() {
-  toggle = !toggle;
-  if (toggle) {
-    iconClose.style.display = "block";
-    navbarToggleIcon.style.display = "none";
-  } else {
-    iconClose.style.display = "none";
-    navbarToggleIcon.style.display = "block";
-  }
+    toggle = !toggle;
+    if (toggle) {
+        iconClose.style.display = "block";
+        navbarToggleIcon.style.display = "none";
+    } else {
+        iconClose.style.display = "none";
+        navbarToggleIcon.style.display = "block";
+    }
 }
 
 // gestion de l'affichage de la navbar
@@ -24,19 +24,18 @@ function swipeIcone() {
 let prevScrollPos = window.pageYOffset;
 
 function fixedNav() {
-  const currentScrollPos = window.pageYOffset;
-  const navbar = document.querySelector(".navbar");
+    const currentScrollPos = window.pageYOffset;
+    const navbar = document.querySelector(".navbar");
 
+    if (currentScrollPos < prevScrollPos) {
+        navbar.classList.add("fixed-top");
+        navbar.classList.add("navbar-spawn");
+    } else {
+        navbar.classList.remove("navbar-spawn");
+        navbar.classList.remove("fixed-top");
+    }
 
-  if (currentScrollPos < prevScrollPos) {
-    navbar.classList.add("fixed-top");
-    navbar.classList.add("navbar-spawn");
-  } else {
-    navbar.classList.remove("fixed-top");
-    navbar.classList.remove("navbar-spawn");
-  }
-
-  prevScrollPos = currentScrollPos;
+    prevScrollPos = currentScrollPos;
 }
 
 window.addEventListener("scroll", fixedNav);
